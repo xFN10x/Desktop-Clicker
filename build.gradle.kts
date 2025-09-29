d6bcbb5c50154cc2b78f3cf141ebbf2759d94868
_FN10_ <mathdudex09@gmail.com>
2025-09-29 17:33:46 -0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    application
+    id("com.gradleup.shadow") version "9.2.2"
+    id("org.panteleyev.jpackageplugin") version "1.7.6"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava)
+
+    implementation("com.google.code.gson:gson:2.13.2")
+    implementation("com.formdev:flatlaf:3.6.1")
+    implementation("commons-io:commons-io:2.19.0")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "fn10.desktopClicker.Launcher"
+}
+
+val version = "1.0"
+
+tasks.shadowJar {
+  archiveBaseName = "3dsUnistoreCreator"
+  archiveVersion = version
+  destinationDirectory = layout.buildDirectory.dir("builtJars")
+}
---
+++ src/main/java/fn10/desktopClicker/Launcher.java
@@ -0,0 +1,12 @@
+package main.java.fn10.desktopClicker;
+
+import javax.swing.JFrame;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("test");
+
+        jFrame.setBackground(new Color(0,0,0,0));
+    }
+}
